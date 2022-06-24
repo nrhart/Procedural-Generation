10afc8299d51b2c2d5a8a12f5482c7b4e872b1e7
Noah Hart <nrhart@ucsc.edu>
2022-06-23 20:49:49 -0700

    Tony

--- P1/README.md
+++ P1/README.md
@@ -1,11 +1,16 @@
 # p1-Tony's Demise
+## Noah Hart
 
 Tony's Demise takes the templates from the starter code and adds it's own theme,
 which sort of mimics how some games have boss drop pools. So, the goal is for
 a person to fight their way to the One Tony and kill to get a unqiue item
 from his loot pool. Think of Tony as say... Morty from "Rick and Morty."
+The catch is that the Tony at the top of the mountain as created clones of himself,
+that act as bosses that spread across the land, which opens up the possibilty of 
+turning this into an actualgame
 
-In this assignment I replaced many words throughout relating to loot, the name of the towns, and
-changed the adventure category to the Tony category to build into the theme I was trying to create.
+In this assignment I focused on creating a setting which is the mountain pass, giving 
+the enemies various names an adjectives. The main draw is the weapons the player is given
+and the lootpool of the boss, which opens the possibility of deep lore implications.
 
 Credit: Adam Smith
--- P1/script.js
+++ P1/script.js
@@ -1,29 +1,22 @@
 const fillers = {
     Tony: ["ynoT", "Tonietta", "Tony", "Brother of Tony", "Sister of Tony", "$Tony and $Tony", "$Tony, $Tony, and $Tony", "Brother of Tony", "Sister of Tony"],
-    pre: ["Ton", "Bre", "Man", "Trot", "Glen", "Glo"],
-    post: ["y", "i", "-by-the-sea" ,"dale", "ora", "amas", "acity"],
-    people: ["dastardly", "spooky", "gigantic", "hidden", "clumsy", "half-hearted", "mysterious", "unpredictable", "malicious"],
-    item: ["double bladed Tony", "sword of the slayen Tony", "book about Tony", "cape of Tony", "crest fallen shield of Tony", "Tony's number one weakness"],
-    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    loots: ["coins", "chalices of Tony's blood", "Tony's bones", "Tony's iconic hand", "Exp Points", "The Heart of Tony", "maybe a princess?"],
-    baddies: ["Evil Tony's", "Ex-Tony's", "Sea-Tony's", "Sky-Tony's", "mountain Tony's", "Grimlins", "Pepa pigs", "evil $Tony", "agents of the Tony brotherhood"],
-    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
+    transcended: ["The One Tony", "Tony who remains", "The Almighty Tony", "The most powerful Tony of all"],
+    town: ["Tony-by-the-sea", "Tonacity", "Ariamas", "Rotsworth", "Glendale", "Glowhoo"],
+    townfolk: ["dastardly", "spooky", "gigantic", "hidden", "clumsy", "half-hearted", "mysterious", "unpredictable", "malicious"],
+    weapon: ["double bladed Tony", "sword of the slayen Tony", "book about Tony", "cape of Tony", "crest fallen shield of Tony", "Tony's number one weakness"],
+    lootpool: ["coins", "chalices of Tony's blood", "Tony's bones", "Tony's iconic hand", "Exp Points", "The Heart of Tony", "maybe a princess?"],
+    foes: ["Evil Tony's", "Ex-Tony's", "Sea-Tony's", "Sky-Tony's", "mountain Tony's", "Grimlins", "Pepa pigs", "Evil $Tony", "Agents of the Tony Brotherhood"],
+    amount: ["a plethora", "way to many to count", "an insane", "a good amount", "an unknown", "many"],
     
   };
   
-  /*const template = `$Tony, heed my $message!
+  const template = `$Tony, you have been tasked to journey through the mountains to take out $transcended!
   
-  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
-  
-  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
-  `;
-  */
-  const template = `$Tony, pay regard to my $message!
-  
-  There is a place called $pre$post over the mountain, where the $people will stop at nothing to help their One Tony transcend. The road is crawling with $baddies and is up to you to get through them and stop the ritual.
+  There is a place called $town over the mountain, where the $townfolk will stop at nothing to help their One Tony transcend. The pass is crawling with $foes and is up to you to get through them and stop the ritual.
 
-  Take this $item to help yourself fight through the mountain pass and destroy the One Tony once and for all. It is said that you will be rewarded with $num $looty $loots for your troubles`;
+  Take this $weapon to help yourself fight through the mountain pass and destroy the One Tony once and for all. It is said that you will be rewarded with $lootpool for your troubles.
+  
+  It is unknown if and when the ritual will be completed, but if you are too late, then $amount of Powerful Tony's will be released accross the lands.`;
   
   // STUDENTS: You don't need to edit code below this line.
   
