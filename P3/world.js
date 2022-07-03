acd89a23f74716e699607481b914ebbd28a77d0e
Noah Hart <nrhart@ucsc.edu>
2022-07-02 21:15:21 -0700

    autotiling fixing

--- P3/world.js
+++ P3/world.js
@@ -27,10 +27,10 @@ function p3_worldKeyChanged(key) {
 }
 
 function p3_tileWidth() {
-  return 16;
+  return 64;
 }
 function p3_tileHeight() {
-  return 16;
+  return 64;
 }
 
 let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
@@ -46,31 +46,63 @@ function p3_tileClicked(i, j) {
 function p3_drawBefore() {}
 
 function p3_drawTile(i, j) {
-  noStroke();
-  fill(noise(i, j) * 255)
+  stroke(255);
+  let tile = noise(i,j) * 3;
 
   push();
-
-  beginShape();
-  vertex(0, 0);
-  vertex(0, tw);
-  vertex(th, tw);
-  vertex(th, 0);
-  ellipse(0,0);
-  endShape(CLOSE);
+  if(tile < 1){
+    push();
+    drawpurple();
+    if(noise(i-1, j) * 3 >= 1 && noise(i-1,j) * 3 < 2){ //west
+      push();
+      autotileredwest();
+      pop();
+    }
+    if(noise(i+1, j) * 3 >= 1 && noise(i+1,j) * 3 < 2){ //east
+      push();
+      autotileredeast();
+      pop();
+    }
+    if(noise(i-1, j) * 3 >= 1 && noise(i,j-1) * 3 < 2){ //north
+      push();
+      autotilerednorth();
+      pop();
+    }
+    if(noise(i-1, j) * 3 >= 1 && noise(i,j+1) * 3 < 2){ //south
+      push();
+      autotileredsouth();
+      pop();
+    }
+    pop();
+  } else if(tile < 2){
+    push();
+    drawpinkred();
+    pop();  
+  } else{
+    push();
+    drawyellow();
+    pop();
+  }
 
   let n = clicks[[i, j]] | 0;
   if (n % 2 == 1) {
-    fill(255, 255, 0, 180);
-    ellipse(th/2, tw/2, 10, 10);
+    fill(0, 255, 255, 180);
+    beginShape();
+    vertex(0, 0);
+    vertex(0, tw);
+    vertex(th, tw);
+    vertex(th, 0);
+    ellipse(0,0);
+    endShape(CLOSE);
   }
 
   pop();
+  
 }
 
 function p3_drawSelectedTile(i, j) {
   noFill();
-  stroke(0, 255, 0, 128);
+  stroke(0, 255, 255, 128);
 
   beginShape();
   vertex(0, 0);
@@ -85,3 +117,80 @@ function p3_drawSelectedTile(i, j) {
 }
 
 function p3_drawAfter() {}
+
+function drawpinkred(){
+  fill('#db1665')
+  noStroke();
+  beginShape();
+  vertex(0, 0);
+  vertex(0, tw);
+  vertex(th, tw);
+  vertex(th, 0);
+  ellipse(0,0);
+  endShape(CLOSE);
+}
+
+function drawpurple(){
+  fill('#9a16db')
+  noStroke();
+  beginShape();
+  vertex(0, 0);
+  vertex(0, tw);
+  vertex(th, tw);
+  vertex(th, 0);
+  ellipse(0,0);
+  endShape(CLOSE);
+}
+
+function drawyellow(){
+  fill('#e1eb34')
+  noStroke();
+  beginShape();
+  vertex(0, 0);
+  vertex(0, tw);
+  vertex(th, tw);
+  vertex(th, 0);
+  ellipse(0,0);
+  endShape(CLOSE);
+}
+
+function autotileredwest(){
+  fill('#db1665')
+  noStroke();
+  beginShape();
+  vertex(0,0);
+  vertex(16,0);
+  vertex(16,64);
+  vertex(0,64);
+  endShape();
+}
+function autotileredeast(){
+  fill('#db1665')
+  noStroke();
+  beginShape();
+  vertex(64,0);
+  vertex(48,0);
+  vertex(48,64);
+  vertex(64,64);
+  endShape();
+}
+function autotilerednorth(){
+  fill('#db1665')
+  noStroke();
+  beginShape();
+  vertex(0,64);
+  vertex(0,16);
+  vertex(64,16);
+  vertex(64,0);
+  endShape();
+}
+function autotileredsouth(){
+  fill('#db1665')
+  noStroke();
+  beginShape();
+  vertex(0,0);
+  vertex(0,16);
+  vertex(64,16);
+  vertex(64,0);
+  endShape();
+}
